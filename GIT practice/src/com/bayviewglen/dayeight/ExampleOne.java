12cc9573125fc03ee3b7f4501b585a3ab60b4bf1
hapitz <hapitz@bayviewglen.ca>
2014-09-08 13:50:09 -0400

    changed pos int to 12

--- GIT practice/src/com/bayviewglen/dayeight/ExampleOne.java
+++ GIT practice/src/com/bayviewglen/dayeight/ExampleOne.java
@@ -3,7 +3,7 @@ package com.bayviewglen.dayeight;
 public class ExampleOne {
 
 	public static void main(String[] args) {
-		final int POSITIVE_INTEGER = 5;
+		final int POSITIVE_INTEGER = 12;
 		final int NEGATIVE_INTEGER = -8;
 		final int ZERO = 0;
 		
