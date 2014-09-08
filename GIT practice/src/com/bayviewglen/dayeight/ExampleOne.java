7205a5363d0725e73567d632a15a448c688e5e15
hapitz <hapitz@bayviewglen.ca>
2014-09-08 13:45:23 -0400

    Changed positive double to 7.0

--- GIT practice/src/com/bayviewglen/dayeight/ExampleOne.java
+++ GIT practice/src/com/bayviewglen/dayeight/ExampleOne.java
@@ -7,7 +7,7 @@ public class ExampleOne {
 		final int NEGATIVE_INTEGER = -9;
 		final int ZERO = 0;
 		
-		final double POSITIVE_DOUBLE = 5.0;
+		final double POSITIVE_DOUBLE = 7.0;
 		final double NEGATIVE_DOUBLE = -8.0;
 		
 	//	System.out.println(POSITIVE_INTEGER/ZERO);
